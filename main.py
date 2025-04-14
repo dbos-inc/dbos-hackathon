d12bfb5777f6bbb7e3b8b5cc80953e3a4bfa6313
Peter Kraft <petereliaskraft@gmail.com>
2025-04-14 13:29:57 -0700

    start

---
+++ main.py
@@ -0,0 +1,123 @@
+import logging
+import os
+from concurrent.futures import ThreadPoolExecutor
+from tempfile import TemporaryDirectory
+from typing import List
+
+import requests
+import uvicorn
+from fastapi import FastAPI, Response, status
+from llama_index.core import Settings, StorageContext, VectorStoreIndex
+from llama_index.readers.file import PDFReader
+from llama_index.vector_stores.postgres import PGVectorStore
+from pydantic import BaseModel, HttpUrl
+from sqlalchemy import create_engine, make_url, text
+
+app = FastAPI()
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger("dbos-hackathon")
+
+###########################
+# Configure Vector Index
+###########################
+
+db_url = os.environ.get("DBOS_DATABASE_URL", "postgresql://postgres:dbos@localhost:5432/dbos_hackathon")
+
+def configure_index():
+    Settings.chunk_size = 512
+    url = make_url(db_url)
+    # Create the index database if it does not exist
+    conn_string = f"postgresql://{url.username}:{url.password}@{url.host}:{url.port}/postgres"
+    engine = create_engine(conn_string)       
+    with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
+        result = conn.execute(text(f"SELECT 1 FROM pg_database WHERE datname = '{url.database}'"))
+        db_exists = result.scalar() is not None
+        if not db_exists:
+            conn.execute(text(f"CREATE DATABASE {url.database}"))
+            print(f"Database '{url.database}' created successfully.")
+    # Create the vector index
+    vector_store = PGVectorStore.from_params(
+        database=url.database,
+        host=url.host,
+        password=url.password,
+        port=url.port,
+        user=url.username,
+    )
+    vector_store._initialize()
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = VectorStoreIndex([], storage_context=storage_context)
+    chat_engine = index.as_chat_engine()
+    return index, chat_engine
+
+
+index, chat_engine = configure_index()
+
+###########################
+# Index Documents
+###########################
+
+executor = ThreadPoolExecutor()
+
+
+def index_documents(urls: List[HttpUrl]):
+    indexed_pages = 0
+    futures = []
+    for url in urls:
+        future = executor.submit(index_document, url)
+        futures.append(future)
+    for future in futures:
+        indexed_pages += future.result()
+    logger.info(f"Indexed {len(urls)} documents totaling {indexed_pages} pages")
+    return indexed_pages
+
+
+def index_document(document_url: HttpUrl) -> int:
+    with TemporaryDirectory() as temp_dir:
+        temp_file_path = os.path.join(temp_dir, "file.pdf")
+        with open(temp_file_path, "wb") as temp_file:
+            with requests.get(document_url, stream=True) as r:
+                r.raise_for_status()
+                for page in r.iter_content(chunk_size=8192):
+                    temp_file.write(page)
+            temp_file.seek(0)
+            reader = PDFReader()
+            pages = reader.load_data(temp_file_path)
+    for page in pages:
+        index.insert(page)
+    return len(pages)
+
+
+class URLList(BaseModel):
+    urls: List[HttpUrl]
+
+
+@app.post("/index")
+def index_endpoint(urls: URLList):
+    executor.submit(index_documents, urls.urls)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+###########################
+# Query Index
+###########################
+
+
+class ChatSchema(BaseModel):
+    message: str
+
+
+@app.post("/chat")
+def chat_workflow(chat: ChatSchema):
+    response = query_model(chat.message)
+    return {"content": response}
+
+
+def query_model(message: str) -> str:
+    return str(chat_engine.chat(message))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
